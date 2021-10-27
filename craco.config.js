28f2891ee9e1ac5ed3b8673d7f34434ec015d367
kingford <32887862+skingford@users.noreply.github.com>
2021-10-27 09:05:05 +0800

    chore: 构建进度条 & 打包的进度

--- craco.config.js
+++ craco.config.js
@@ -1,27 +1,48 @@
 /*
  * @Author: kingford
  * @Date: 2021-10-25 19:46:10
- * @LastEditTime: 2021-10-26 19:20:29
+ * @LastEditTime: 2021-10-26 20:54:44
  */
 const { whenDev } = require("@craco/craco");
 const fastRefreshCracoPlugin = require("craco-fast-refresh");
+
+const webpackBar = require("webpackbar");
+const simpleProgressWebpackPlugin = require("simple-progress-webpack-plugin");
+
 const path = require("path");
 const pathResolve = (pathUrl) => path.join(__dirname, pathUrl);
 
 module.exports = {
-  // 配置别名
+  // webpack 配置
   webpack: {
+    // 配置别名
     alias: {
       "@": pathResolve("src"),
     },
+    plugins: [
+      // webpack构建进度条
+      new webpackBar({
+        profile: true,
+      }),
+
+      // 查看打包的进度
+      new simpleProgressWebpackPlugin(),
+    ],
   },
+
+  // 配置插件
   plugins: [
-    ...whenDev(() => [
-      {
-        plugin: fastRefreshCracoPlugin,
-      },
-    ]),
+    ...whenDev(
+      () => [
+        {
+          plugin: fastRefreshCracoPlugin,
+        },
+      ],
+      []
+    ),
   ],
+
+  // 配置开发服务
   devServer: {
     port: 9000,
     proxy: {
---
+++ src/setting/env.ts
@@ -0,0 +1,7 @@
+/*
+ * @Author: kingford
+ * @Date: 2021-10-26 20:44:48
+ * @LastEditTime: 2021-10-26 20:45:58
+ */
+export const IS_PROD = process.env.NODE_ENV === "production";
+export const IS_DEV = process.env.NODE_ENV === "development";
