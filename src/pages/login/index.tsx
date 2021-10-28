448f13ee0210fb416b9c3b1ff8d8580803033e99
kingford <32887862+skingford@users.noreply.github.com>
2021-10-28 20:57:55 +0800

    chore: debugger of vscode

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,14 @@
 /*
  * @Author: kingford
  * @Date: 2021-10-25 20:31:13
- * @LastEditTime: 2021-10-25 20:33:38
+ * @LastEditTime: 2021-10-28 20:56:47
  */
 const Login = () => {
-  return <div>Login</div>;
+  const login = () => {
+    console.log("login");
+  };
+
+  return <div onClick={login}>Login</div>;
 };
 
 export default Login;
